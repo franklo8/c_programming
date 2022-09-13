0a574d6126da3281dd782a9b34321a45d90c0c83
franklo8 <franklinejeh@gmail.com>
2022-09-13 13:14:45 +0100

    personal projects

---
+++ c/02L01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+main()
+{
+	printf("Howdy, neighbor! This is my first C progran.\n");
+	return 0;
+}
---
+++ c/exercise3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main()
+
+{
+	printf ("Howdy, neighbor!\nThis is my first C program.\n");
+	return 0;
+
+}
